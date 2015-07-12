fdd7fa0dd4504cf5ada9d2c6a2aa1ec0a31bb018
Zeeus80 <zeeus80@gmail.com>
2015-07-12 23:51:56 +0200

    Despliege en Heroku

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res) {
 //  res.render('index', { title: 'Express' });  Cambio Primera pagina
   res.render('index', { title: 'Quiz' });  // Cambio primera pagina
 });
+
+router.get('/quizes/question', quizController.question);
+router.get('/quizes/answer',   quizController.answer);
+
 
 module.exports = router;
