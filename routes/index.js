eb102b5550bd580b378936521fd6501420a268dd
Zeeus80 <zeeus80@gmail.com>
2015-07-12 21:59:22 +0200

    Primera página
    Primera página

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+//  res.render('index', { title: 'Express' });  Cambio Primera pagina
+  res.render('index', { title: 'Quiz' });  // Cambio primera pagina
 });
 
 module.exports = router;
